e43af714010bc2583930a25e3ced5df42ca402e0
Bas Wilbers <baswilbers@gmail.com>
2009-04-10 18:01:08 +0200

    Initial modify to make it compatible with GAE

--- lib/pony.rb
+++ lib/pony.rb
@@ -1,72 +1,25 @@
-require 'rubygems'
-require 'net/smtp'
-begin
-	require 'tmail'
-rescue LoadError
-	require 'actionmailer'
-end
+require 'java'
 
 module Pony
+	module MS
+	  import com.google.appengine.api.mail.MailServiceFactory 
+	  import com.google.appengine.api.mail.MailService 
+	  Service = MailServiceFactory.mail_service
+	end
+	
 	def self.mail(options)
-		raise(ArgumentError, ":to is required") unless options[:to]
-
-		via = options.delete(:via)
-		if via.nil?
-			transport build_tmail(options)
-		else
-			if via_options.include?(via.to_s)
-				send("transport_via_#{via}", build_tmail(options), options)
-			else
-				raise(ArgumentError, ":via must be either smtp or sendmail")
-			end
-		end
+	  raise(ArgumentError, ":to is required") unless options[:to]
+		raise(ArgumentError, ":from is required") unless options[:from]
+		transport_via_gae build_gae_mail(options)
 	end
-
-	def self.build_tmail(options)
-		mail = TMail::Mail.new
-		mail.to = options[:to]
-		mail.from = options[:from] || 'pony@unknown'
-		mail.subject = options[:subject]
-		mail.body = options[:body] || ""
-		mail
-	end
-
-	def self.sendmail_binary
-		@sendmail_binary ||= `which sendmail`.chomp
-	end
-
-	def self.transport(tmail)
-		if File.executable? sendmail_binary
-			transport_via_sendmail(tmail)
-		else
-			transport_via_smtp(tmail)
-		end
-	end
-
-	def self.via_options
-		%w(sendmail smtp)
-	end
-
-	def self.transport_via_sendmail(tmail, options={})
-		IO.popen('-', 'w+') do |pipe|
-			if pipe
-				pipe.write(tmail.to_s)
-			else
-				exec(sendmail_binary, *tmail.to)
-			end
-		end
-	end
-
-	def self.transport_via_smtp(tmail, options={:smtp => {}})
-		default_options = {:smtp => { :host => 'localhost', :port => '25', :domain => 'localhost.localdomain' }}
-		o = default_options[:smtp].merge(options[:smtp])
-		smtp = Net::SMTP.new(o[:host], o[:port])
-		if o.include?(:auth)
-			smtp.start(o[:domain], o[:user], o[:password], o[:auth])
-		else
-			smtp.start(o[:domain])
-		end
-		smtp.send_message tmail.to_s, tmail.from, tmail.to
-		smtp.finish
+	def self.transport_via_gae mail
+		MS::Service.send(mail)
+	end	
+	def self.build_gae_mail(options)
+		to = options[:to]
+		from = options[:from]
+		subject = options[:subject]
+		body = options[:body] || " "
+		mail = MS::MailService::Message.new(from,to,subject,body)
 	end
 end
