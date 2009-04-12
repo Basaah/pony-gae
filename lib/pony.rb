13c9c64270af1b1420fa98659a697ef342b3ff8c
Bas Wilbers <baswilbers@gmail.com>
2009-04-12 17:33:05 +0200

    Added all functions from my todo list

--- lib/pony.rb
+++ lib/pony.rb
@@ -10,16 +10,34 @@ module Pony
 	def self.mail(options)
 	  raise(ArgumentError, ":to is required") unless options[:to]
 		raise(ArgumentError, ":from is required") unless options[:from]
-		transport_via_gae build_gae_mail(options)
+		MS::Service.send build_gae_mail(options)
 	end
-	def self.transport_via_gae mail
-		MS::Service.send(mail)
-	end	
+	def self.mail_to_admins(options)
+		MS::Service.sendToAdmins build_gae_mail(options)
+	end
+
 	def self.build_gae_mail(options)
-		to = options[:to]
-		from = options[:from]
-		subject = options[:subject]
+	  mail = MS::MailService::Message.new
+		mail.setTo(options[:to].to_a)
+		mail.setSender(options[:from])
+		mail.setSubject(options[:subject]) if options[:subject]
 		body = options[:body] || " "
-		mail = MS::MailService::Message.new(from,to,subject,body)
+		if options[:html_body] == true
+      mail.setHtmlBody(body)
+    else
+      mail.setTextBody(body)
+    end    
+    mail.setReplyTo(options[:reply_to]) if options[:reply_to]
+    mail.setCc(options[:cc].to_a) if options[:cc]
+    mail.setBcc(options[:bcc].to_a) if options[:bcc]
+    if options[:attachments]
+      attachments = []
+      options[:attachments].each do |filename,data|
+        attachments << MS::MailService::Attachment.new(filename,data.to_java_bytes)
+      end	        
+      mail.setAttachments(attachments)
+    end  
+    mail   
 	end
 end
+
